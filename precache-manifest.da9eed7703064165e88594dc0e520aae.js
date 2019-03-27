4c1272e226aab40d22e67f8a369ab7af023ed666
gastonpereyra <rgpxen@gmail.com>
2019-03-26 22:01:56 -0300

    Empezar

---
+++ precache-manifest.da9eed7703064165e88594dc0e520aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8e5481308a54a93d50f",
+    "url": "/static/css/main.170cc1a8.chunk.css"
+  },
+  {
+    "revision": "c8e5481308a54a93d50f",
+    "url": "/static/js/main.615b912c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6423ba81476108a4404b798a5bebdbe",
+    "url": "/index.html"
+  }
+];
