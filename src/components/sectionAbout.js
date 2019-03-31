00f9ab4b76ab6774b0205a5fdcef28ac3f663829
gastonpereyra <rgpxen@gmail.com>
2019-03-30 21:50:14 -0300

    Mejorar componente About

--- src/components/sectionAbout.js
+++ src/components/sectionAbout.js
@@ -2,34 +2,34 @@ import React from 'react';
 
 const acercaInfo = [
     {
-        image: "assets/ani_kraken.png",
-        title: "Comenzamos",
-        text: "sdfasdfa sdfasd fasd fasd fasd fasd fas dfa sdf asd fasdf asdkf asdk fasdk fasdfasd fasd fas dfasdf asdf asdf asdf asdf asd fas df"
+        // image: "",
+        title: "Milenarios",
+        text: "Nuestra historia comienza milenios atrás, un grupo de familias se reunió para acordar proteger y estudiar criaturas fantasticas."
     },
     {
-        image: "assets/ani_krake.png",
-        title: "Comenzamos",
-        text: "sdfasdfa sdfasd fasd fasd fasd fasd fas dfa sdf asd fasdf asdkf asdk fasdk fasdfasd fasd fas dfasdf asdf asdf asdf asdf asd fas df"
+        
+        title: "Globales",
+        text: "Estamos presentes en todos los continentes, en cientos de paises y ciudades, listos y preparados para atender en cualquier momento."
     },
     {
-        title: "Comenzamos",
-        text: "sdfasdfa sdfasd fasd fasd fasd fasd fas dfa sdf asd fasdf asdkf asdk fasdk fasdfasd fasd fas dfasdf asdf asdf asdf asdf asd fas df"
+        title: "Especialistas",
+        text: "Nuestros profesionales son los mejores en su especialidad, estan en constante capacitación y aman con locura lo que hacen."
     },
     {
-        image: "assets/ani_kraken.png",
-        title: "Comenzamos",
-        text: "sdfasdfa sdfasd fasd fasd fasd fasd fas dfa sdf asd fasdf asdkf asdk fasdk fasdfasd fasd fas dfasdf asdf asdf asdf asdf asd fas df"
+        
+        title: "Responsables",
+        text: "No solo cuidamos y protegemos a los animales mas vulnerables, también al ecosistema en el que viven y a la comunidad que pertenecen."
     },
     {
-        image: "assets/ani_krake.png",
-        title: "Comenzamos",
-        text: "sdfasdfa sdfasd fasd fasd fasd fasd fas dfa sdf asd fasdf asdkf asdk fasdk fasdfasd fasd fas dfasdf asdf asdf asdf asdf asd fas df"
+        
+        title: "Ayuda Oficial",
+        text: "Además de nuestra labor privada, proveemos ayuda oficial para la protección de parques nacionales, y atención médica veteriana."
     }
 ]
 
 const InfoBlock = ({imageRoute=null,title='',text=''}) => {
     return (
-        <>
+        <div className="box">
             {imageRoute && <figure className="image is-64x64 is-image-center">
                 <img src={imageRoute} alt="Block Card"/>
             </figure>}
@@ -37,7 +37,7 @@ const InfoBlock = ({imageRoute=null,title='',text=''}) => {
                 <p className="is-size-3"><u>{title}</u></p>
                 <p className="is-size-4">{text}</p>
             </div>
-        </>
+        </div>
     )
 }
 
@@ -49,10 +49,10 @@ const About = () => {
                     <h2 className="title    ">
                         ¿ Quienes Somos ? 
                     </h2>
-                    <div className="columns is-multiline">
+                    <div className="columns is-multiline is-centered">
                         
                         { acercaInfo.map( (a,i) => (
-                            <div className="column has-text-centered" key={i}>
+                            <div className="column is-one-third has-text-centered" key={i}>
                                 <InfoBlock imageRoute={a.image} title={a.title} text={a.text} />
                             </div>
                         ))}
