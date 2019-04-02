74468a2ac67c2f13ef2e46d0e73c5b9272ebe197
gastonpereyra <rgpxen@gmail.com>
2019-04-01 22:46:07 -0300

    Empezar a editar componente Proffesionals

--- src/components/sectionProffesionals.js
+++ src/components/sectionProffesionals.js
@@ -8,6 +8,25 @@ const Profesionals = () => {
                     <p className="title">
                         Profesionales
                     </p>
+
+                    <article className="media">
+                    <figure className="media-left">
+                        <p className="image is-64x64">
+                        <img className="is-rounded" src="https://pickaface.net/gallery/avatar/myrosith51dfd4eada6e3.png" alt="Avatar"/>
+                        </p>
+                    </figure>
+                        <div className="media-content">
+                            <div className="content">
+                                <p>
+                                    <strong>Angeles Yoli</strong> <small>@AngieYoli</small>
+                                    <br/>
+                                        No sabes lo que es tomar café hasta que llegas y aprendes, es una experiencia religiosa, te cambia la vida, CREEME!
+                                    <br/><b className="has-text-link">#Café101</b>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+
                 </div>
             </div>
         </section>
